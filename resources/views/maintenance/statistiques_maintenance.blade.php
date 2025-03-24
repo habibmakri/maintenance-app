035e6bb3e4acd27c41573c97989e4e4bd909cb82
habibmakri <habi2014mak@gmail.com>
2025-03-24 10:02:22 +0100

    adding statistiques maintenance 20

--- resources/views/maintenance/statistiques_maintenance.blade.php
+++ resources/views/maintenance/statistiques_maintenance.blade.php
@@ -5,11 +5,11 @@
 @section('content')
     <style>
         /* @font-face {
-                                                                    font-family: 'lateef';
-                                                                    src: url('{{ asset('theme/fonts/lateef/Lateef-Regular.ttf') }}') format('truetype');
-                                                                    font-weight: normal;
-                                                                    font-style: normal;
-                                                                } */
+                                                                        font-family: 'lateef';
+                                                                        src: url('{{ asset('theme/fonts/lateef/Lateef-Regular.ttf') }}') format('truetype');
+                                                                        font-weight: normal;
+                                                                        font-style: normal;
+                                                                    } */
         label {
             inset-inline-end: auto !important;
         }
@@ -156,76 +156,77 @@
                     });
                 });
             </script>
-        </div>
-        <div style="border-bottom: solid;border-block-width: 2px;padding-bottom: 10px;margin-bottom:15px;">
-            <h5 class="mt-5">Sélectionner le Bus et la piece et l'année pour la récupuration des données:</h5>
-            <form class="row g-3" action="{{ route('app.maintenance.etat_piece_pdf') }}" method="post">
-                @csrf
-                <input type="hidden" name="data_type" id="data_type" value="ligne_bus_mois">
-                <div class="col-md-4">
-                    <div class="form-floating">
-                        <select class="form-select" required name="bus" id="bus"
-                            aria-label="Floating label select example">
-                            <option value="" disabled selected>Sélectionner le Bus</option>
-                            @foreach ($buses as $bus)
-                                <option value="{{ $bus->id }}">{{ $bus->name }}</option>
-                            @endforeach
 
-                        </select>
-                        <label for="bus">Bus</label>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-floating">
-                        <select class="form-select" required name="piece2" id="piece2"
-                            aria-label="Floating label select example">
-                            <option value="" disabled selected>Sélectionner la piece</option>
-                            <option value="Gasoile">Gasoile</option>
-                            <option value="Kilometrage">Kilometrage</option>
-                            <option value="Gasoile/100">Gasoile/100</option>
-                            <option value="Huile 15w40">Huile 15w40</option>
-                            <option value="Huile 15w40/Sans vidange">Huile 15w40/Sans vidange</option>
-                            <option value="Glaciole">Glaciole</option>
-                        </select>
-                        <label for="piece">Piece</label>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="form-floating">
-                        <select class="form-select" required name="year2" id="year2"
-                            aria-label="Floating label select example">
-                            <option value="" disabled selected>Sélectionner l'année</option>
-                            @for ($i = date('Y'); $i >= 2024; $i--)
-                                <option value="{{ $i }}">{{ $i }}</option>
-                            @endfor
-                        </select>
-                        <label for="year">Année</label>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div id="lineChart" style="min-height: 400px;" class="echart"></div>
+            <div style="border-bottom: solid;border-block-width: 2px;padding-bottom: 10px;margin-bottom:15px;">
+                <h5 class="mt-5">Sélectionner le Bus et la piece et l'année pour la récupuration des données:</h5>
+                <form class="row g-3" action="{{ route('app.maintenance.etat_piece_pdf') }}" method="post">
+                    @csrf
+                    <input type="hidden" name="data_type" id="data_type" value="ligne_bus_mois">
+                    <div class="col-md-4">
+                        <div class="form-floating">
+                            <select class="form-select" required name="bus" id="bus"
+                                aria-label="Floating label select example">
+                                <option value="" disabled selected>Sélectionner le Bus</option>
+                                @foreach ($buses as $bus)
+                                    <option value="{{ $bus->id }}">{{ $bus->name }}</option>
+                                @endforeach
 
-        <script>
-            document.addEventListener("DOMContentLoaded", () => {
-                echarts.init(document.querySelector("#lineChart")).setOption({
-                    xAxis: {
-                        type: 'category',
-                        data: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août',
-                            'Septembre', 'Octobre', 'Novembre', 'Décembre'
-                        ]
-                    },
-                    yAxis: {
-                        type: 'value'
-                    },
-                    series: [{
-                        data: [],
-                        type: 'line',
-                        // smooth: true
-                    }]
+                            </select>
+                            <label for="bus">Bus</label>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-floating">
+                            <select class="form-select" required name="piece2" id="piece2"
+                                aria-label="Floating label select example">
+                                <option value="" disabled selected>Sélectionner la piece</option>
+                                <option value="Gasoile">Gasoile</option>
+                                <option value="Kilometrage">Kilometrage</option>
+                                <option value="Gasoile/100">Gasoile/100</option>
+                                <option value="Huile 15w40">Huile 15w40</option>
+                                <option value="Huile 15w40/Sans vidange">Huile 15w40/Sans vidange</option>
+                                <option value="Glaciole">Glaciole</option>
+                            </select>
+                            <label for="piece">Piece</label>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-floating">
+                            <select class="form-select" required name="year2" id="year2"
+                                aria-label="Floating label select example">
+                                <option value="" disabled selected>Sélectionner l'année</option>
+                                @for ($i = date('Y'); $i >= 2024; $i--)
+                                    <option value="{{ $i }}">{{ $i }}</option>
+                                @endfor
+                            </select>
+                            <label for="year">Année</label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div id="lineChart" style="min-height: 400px;" class="echart"></div>
+
+            <script>
+                document.addEventListener("DOMContentLoaded", () => {
+                    echarts.init(document.querySelector("#lineChart")).setOption({
+                        xAxis: {
+                            type: 'category',
+                            data: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août',
+                                'Septembre', 'Octobre', 'Novembre', 'Décembre'
+                            ]
+                        },
+                        yAxis: {
+                            type: 'value'
+                        },
+                        series: [{
+                            data: [],
+                            type: 'line',
+                            // smooth: true
+                        }]
+                    });
                 });
-            });
-        </script>
+            </script>
+        </div>
         <div class="tab-pane fade" id="bordered-home" role="tabpanel" aria-labelledby="home-tab">
 
         </div>
@@ -333,11 +334,12 @@
                             let monthIndex = parseInt(item.month.split('-')[1], 10) - 1;
                             yValues[monthIndex] = item.total;
                         });
-
+                        const totalvals = data.map(item => item.total_gasoile);
+                        const total = yValues.reduce((sum, val) => sum + val, 0);
 
                         chart.setOption({
                             title: {
-                                text: piece + ' Du ' + selectedBusText + ' l\'Année ' + year,
+                                text: piece + ' Du ' + selectedBusText + ' l\'Année ' + year + ' Total: '+total,
                                 left: 'center',
                                 textAlign: 'center',
                             },
